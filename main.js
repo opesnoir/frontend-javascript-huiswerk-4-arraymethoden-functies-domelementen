d339f722f567c433bb853eb6bf1dcdf75ac1ecff
opesnoir <goodsurroundsme@outlook.com>
2022-10-29 14:59:52 +0200

    opdracht 1a tot en met 1d af

--- main.js
+++ main.js
@@ -161,3 +161,43 @@ const inventory = [
     sold: 8,
   },
 ];
+
+// Functionaliteit bouwen
+
+//Opdracht 1 - Array Methoden
+//Opdracht 1a:** Gebruik een array-methode om een array te maken met alle tv-type namen. Log de uitkomst in de
+//console.
+
+const typenameTv = inventory.map((invent) => {
+  return invent.type;
+});
+
+console.log(typenameTv);
+
+//Opdracht 1b:** Gebruik een array-methode om alle tv's te verzamelen (de hele objecten) die volledig uitverkocht
+//zijn. Log de uitkomst in de console.
+
+const soldOutTv = inventory.filter((invent) =>{
+  return (invent.originalStock - invent.sold) === 0
+});
+
+console.log(soldOutTv);
+
+//Opdracht 1c:** Gebruik een array-methode om alle tv's te verzamelen (de hele objecten) die over AmbiLight
+//beschikken. Log de uitkomst in de console.
+
+const ambiLightTv = inventory.filter((invent) =>{
+  return invent.options.ambiLight
+});
+
+console.log(ambiLightTv);
+
+//Opdracht 1d:** Schrijf een functie die alle tv's van laagste naar hoogste prijs sorteert. Log de uitkomst in de
+//console.
+
+const sortPriceTv = inventory.sort((a, b) => {
+  return a.price - b.price;
+});
+
+console.log(sortPriceTv);
+
