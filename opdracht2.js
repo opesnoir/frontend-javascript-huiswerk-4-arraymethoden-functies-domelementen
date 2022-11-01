ca95dee06402239874b5997c59be94f2e6029ca0
opesnoir <goodsurroundsme@outlook.com>
2022-11-01 21:31:17 +0100

    opdracht 1 en 3 af

--- opdracht2.js
+++ opdracht2.js
@@ -162,3 +162,17 @@ const inventory = [
   },
 ];
 
+
+// Opdracht 2 - Elementen in de DOM plaatsen
+
+//_Tip_: wanneer we meerdere waardes uit een array willen terugbrengen tot één getal of één string, gebruik je hier gewoon
+//een oude vertrouwde for-loop voor!
+//Opdracht 2a:** Hoeveel tv's zijn er al verkocht? Schrijf een script dat dit berekent. Log de uitkomst in de console.
+//Opdracht 2b:** Zorg ervoor dat dit aantal _in het groen_ wordt weergegeven op de pagina.
+//Opdracht 2c:** Hoeveel tv's heeft Tech It Easy ingekocht? Schrijf een script dat dit berekent. Log de uitkomst in de
+//console.
+//Opdracht 2d:** Zorg ervoor dat dit aantal _in het blauw_ wordt weergegeven op de pagina.
+//Opdracht 2e:** Geef _in het rood_ weer hoeveel tv's er nog verkocht moeten worden.
+
+
+//Einde opdracht 2----------------------
