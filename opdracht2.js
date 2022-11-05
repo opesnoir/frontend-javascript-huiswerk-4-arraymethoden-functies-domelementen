3c92a26d8be5a0c18a74732881e312b96cdf9d5f
opesnoir <goodsurroundsme@outlook.com>
2022-11-05 20:10:06 +0100

    opdracht 2 doorgelopen

--- opdracht2.js
+++ opdracht2.js
@@ -5,54 +5,91 @@
 //een oude vertrouwde for-loop voor!
 //Opdracht 2a:** Hoeveel tv's zijn er al verkocht? Schrijf een script dat dit berekent. Log de uitkomst in de console.
 
-//1. aanpasbare variabel toewijzen die dient als counter;
+// methode 1: for-loop;
+// methode 2: methode met .reduce:
+// methode 3: functie die door de originele array mapt:
+// methode 4: functie die gebruik maakt van een argument om door de parameter te mappen. Als je een parameter aanmaakt, kun je met die parameter gaan werken. Callback is een functie die als argument wordt doorgegeven binnen in een andere functie.
+
+// 2a methode 1: for-loop:
+//1. je wil tellen hoeveel tv's er zijn verkocht. Maak daarvoor een aanpasbare variabel die dient als counter;
 //2. variabel naam voor de counter noteren;
-//3. waarde van de variabel toewijzen, in dit geval 0, gezien de counter leeg moet zijn tijdens het start moment;
-//4. aanpasbare variabel toewijzen die dient als variabel waarmee de objectwaarde wordt aangesproken, in dit geval inventory.sold;
-//5. for-loop schrijven;
-//6. code voor de counter moet de sum van inventory.sold weergeven;
-//7. aanpasbare variabel toewijzen die dient om de sum van inventory.sold op te slaan;
-//8. console.log de variabel met de sum;
-//9. zet het geheel in een functie;
-//10. de parameter voor de functie is soldItems;
-//11. test functie.
+//3. begin waarde van de counter-variabel toewijzen, in dit geval 0, gezien de counter leeg moet zijn tijdens het start moment;
+//4. aanpasbare variabel toewijzen die dient als variabel waarmee de objectwaarde wordt aangesproken, voor in de for {} statement, in dit geval inventory.sold;
+//5. for-loop schrijven: counter > for (){(){}};
+//6. return de sum van inventory.sold;
+//7. console.log de counter ter controle.
 
-// for-loop werkt niet
-/*let counterInitialSumSold = 0;
-
-const soldTvInventory = inventory.map(array => {
-  return array.sold;
-});
+let soldTvCounter = 0;
 
 for (let i = 0; i < inventory.length; i++) {
-let totalSoldTvs = counterInitialSumSold + soldTvInventory;
-}*/
+    soldTvCounter = soldTvCounter + inventory[i].sold;
+}
 
-//methode met .reduce:
+console.log(soldTvCounter);
+
+//2a methode 2: .reduce:
+//reduce loopt door de array element-per-element, bij elke stap voegt de waarde van de huidigeWaarde toe aan de vorigeWaarde.
+//1. je wil tellen hoeveel tv's er zijn verkocht.
+//2. variabel naam voor de counter noteren;
+//3. begin waarde van de counter-variabel toewijzen, in dit geval 0, gezien de counter leeg moet zijn tijdens het start moment;
+//4. aanpasbare variabel toewijzen die dient opslagplaats voor de waarde waarin sum inclusief initiele waarde wordt opgeslagen;
+//5. syntax van de reduce () methode is: declareerbareVariabel [dat zegt of het een let of const is] variabelNaam = teMappenElementNaam.reduce((vorigeWaarde, huidigeWaarde) =>vorigeWaarde + huidigeWaarde, initieleWaarde);
+//6. console.log de counter ter controle.
+
 let totalCount = inventory.reduce((previousValue, currentValue) => previousValue + currentValue.sold, 0);
 console.log(totalCount);
 
-//methode functie die door de originele array mapt:
-function sumSoldTv () {
+//2a methode 3: functie die door de originele array mapt:
+//1. je wil tellen hoeveel tv's er zijn verkocht.
+//2. hiervoor wil je een function maken die door de originele array heenmapt, dat betekent dat je de arraynaam en arraylocatie moet opgeven;
+//3. syntax function: function (){};
+//4. aanpasbare variabel toewijzen die dient opslagplaats voor de waarde waarin sum inclusief initiele waarde wordt opgeslagen;
+//5. return de aanpasbare variabel waarin sum iwordt opgeslagen;
+//4. console.log de functie().
+
+function sumSoldTv() {
     let totalCount = inventory.reduce((previousValue, currentValue) => previousValue + currentValue.sold, 0);
     return totalCount;
 }
 
 console.log(sumSoldTv());
 
-//methode functie die gebruik maakt van een argument om door de parameter te mappen. Als je een parameter aanmaakt, kun je met die parameter gaan werken.
-// callback is een functie die als argument wordt doorgegeven binnen in een andere functie.
-
-//Vraag aan docent / sme'er // de functie "sumSoldTvMetParameter(array)" werkt niet goed, hij logt 0, ik hoor graag waarom dat zo is. Ik zie namelijk niet wat ik fout doe.
-/*function sumSoldTvMetParameter(array) {
-    let totalCount = array.reduce((previousValue, currentValue) => {
-        return previousValue + currentValue.sold, 0;
-    })
-    return totalCount;
+//extra: de functie sumSoldTv() (hierbovenstaande functie) kan je ook inline schrijven, dan krijg je:
+function sumSoldTv2() {
+    return inventory.reduce((previousValue, currentValue) => previousValue + currentValue.sold, 0);
 }
 
-const totalSoldTvs = sumSoldTvMetParameter(inventory);
-console.log(totalSoldTvs);*/
+console.log(sumSoldTv2());
+
+
+//2a methode 4: functie die gebruik maakt van een argument om door de parameter te mappen. Als je een parameter aanmaakt, kun je met die parameter gaan werken.
+// [callback is een functie die als argument wordt doorgegeven binnen in een andere functie]
+//1. je wil tellen hoeveel tv's er zijn verkocht.
+//2. hiervoor wil je een function maken die gebruik maakt van een argument om door de functie heen te mappen. Dat betekent dat je argumenten moet opgeven;
+//3. syntax function: function (){};
+//4. aanpasbare variabel toewijzen die dient opslagplaats voor de waarde waarin sum inclusief initiele waarde wordt opgeslagen;
+//5. return de aanpasbare variabel waarin sum wordt opgeslagen;
+//6. console.log de functie(), en vul binnen de () hetgeen je wil invoeren als argument, om dit binnen de functie te kunnen doorlopen.
+
+function sumSoldTvsWithArgument(array) {
+    let soldTvCounter = 0;
+
+    for (let i = 0; i < array.length; i++) {
+        soldTvCounter = soldTvCounter + array;
+    }
+}
+
+const sumSoldTvsOne = inventory.sold;
+console.log(sumSoldTv2());
+
+//Vraag: in function sumSoldTvMetParameter logt de functie undifined, ik zie niet wat ik verkeerd doe. Weet jij wat er misgaat?
+
+/*function sumSoldTvMetParameter(array) {
+    return array.reduce((previousValue, currentValue) => previousValue + currentValue, 0);
+}
+
+const totalSoldTvsTwo = inventory.sold;
+console.log(totalSoldTvsTwo); //logt undifined, weet niet waarom*/
 
 
 //Opdracht 2b:** Zorg ervoor dat dit aantal _in het groen_ wordt weergegeven op de pagina.
@@ -63,7 +100,7 @@ console.log(totalSoldTvs);*/
 // 5. haal de id van de span op = document.getElementById("verkochteTvsGroeneTekst");
 // 6. geef aan wat je in de amountSoldTv variabel wil opslaan, in dit geval is het de sum van het aantal verkochte tv's. Dat laatste heb je opgeslagen in de methode uit opdracht 2a: totalCount en in de functies uit opdracht 2a: sumSoldTv().
 // 7. syntax voor 4, 5, 6 is: amountSoldTv.textContent = sumSoldTv()
-//als je weet waarom de function sumSoldTvMetParameter(array){} werkt, dan testen met de onderstaande code
+//extra: als je weet waarom de function sumSoldTvMetParameter(array){} werkt, dan testen met de onderstaande code
 
 let amountSoldTv = document.getElementById("soldTvsGreenText")
 amountSoldTv.textContent = sumSoldTv()
@@ -74,13 +111,12 @@ amountSoldTv.textContent = sumSoldTv()
 // methode 1: for-loop;
 // methode 2: methode met .reduce:
 // methode 3: functie die door de originele array mapt:
-// methode 4: functie die gebruik maakt van een argument om door de parameter te mappen. Als je een parameter aanmaakt, kun je met die parameter gaan werken. Callback is een functie die als argument wordt doorgegeven binnen in een andere functie.
+// methode 4: functie die gebruik maakt van een argument om door de parameter te mappen.
 
 // methode 1: for-loop:
 // maak een counter met de beginwaarde van 0;
 // schrijf een for-loop;
 // console.log de variabel om te testen of hij werkt.
-
 
 let counterBoughtTvsForLoop = 0;
 
@@ -92,14 +128,6 @@ console.log(counterBoughtTvsForLoop);
 
 
 // methode 2: methode met .reduce:
-// stappen:
-// 1. maak een variabel waarin je de waarde van de counter opslaat: "let counterBoughtTvsReduceMethode";
-// 2. schrijf een .reduce methode = .reduce((previousValue, currentValue) => previousValue + currentValue, 0);
-// 3. voeg aan de methode, de inventory en de arraylocatie waar de ingekochte tv's zijn opgeslagen (dat is originalStock) toe;
-// 4. schrijf de syntax van de formule inclusief inventory, parameters en originalStock - inventory: inventory.reduce && ((previousValue, currentValue) => && currentValue.originalStock;
-// 5. zet de 'teller' na originalStok op 0 door te schrijven  0);
-// 6. console.log de variabel counterBoughtTvsReduceMethode
-
 
 let counterBoughtTvsReduceMethode = inventory.reduce((previousValue, currentValue) => previousValue + currentValue.originalStock, 0);
 
@@ -107,14 +135,6 @@ console.log(counterBoughtTvsReduceMethode);
 
 
 // methode 3: functie die door de originele array mapt:
-// stappen:
-// 1. schrijf een basisfunctie uit: function functieNaam () {};
-// 2. geef de functie een naam: functionBoughtTvs;
-// 3. laat de parameter leeg, je gaat namelijk door de originele array heenmappen dus dat verwerk je binnen de {};
-// 4. maak een te veranderen variabel (let) om de formule uitkomst in op te slaan, de variabel naam: sumBoughtTvs;
-// 5. schrijf de formule en zet de beginwaarde op 0: .reduce((previousValue, currentValue) => previousValue + currentValue, 0);
-// 6. schrijf de return: return sumBoughtTvs;
-// 7. console.log de functie: functionBoughtTvs ()
 
 function boughtTvsA() {
     let sumBoughtTvs = inventory.reduce((previousValue, currentValue) => previousValue + currentValue.originalStock, 0);
@@ -165,20 +185,5 @@ console.log(stockTv);
 
 let amountStockTvs = document.getElementById("stockTvsRedText")
 amountStockTvs.textContent = stockTv
-
-
-//geeft NAN
-/*function stockTv (boughtTvsA,sumSoldTv) {
-    return boughtTvsA - sumSoldTv
-}
-
-console.log(stockTv());*/
-
-/*function stockTv (boughtTvsA, sumSoldTv){
-    let functionResult = boughtTvsA () - sumSoldTv ()
-}
-
-console.log(stockTv());
-stockTv();*/
 
 //Einde opdracht 2----------------------
